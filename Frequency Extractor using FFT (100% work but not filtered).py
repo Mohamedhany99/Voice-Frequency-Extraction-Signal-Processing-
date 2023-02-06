54f96f59aa88e4b932606d540035f0253a6ce03f
Mohamed Hany <Mohamed.hany182@outlook.com>
2023-02-06 22:57:02 +0200

    Update Frequency Extractor using FFT (100% work but not filtered).py

--- Frequency Extractor using FFT (100% work but not filtered).py
+++ Frequency Extractor using FFT (100% work but not filtered).py
@@ -2,22 +2,16 @@
 """
 Created on Sun Mar 29 00:52:24 2020
 
-@author: aa
+@author: Mohamed Hany
 """
 from scipy.io import wavfile
 #import wave
 #import struct
 import numpy as np
-#first way to enter the wave file data (nframes + frame rate) using the wave lib
-#wave_file = wave.open("HH3.wav",'r')
-#nframes = wave_file.getnframes()
-#data = wave_file.readframes(nframes)
-#frate = wave_file.getframerate
-#wave_file.close()
-#data = struct.unpack('{n}h'.format(n=data_size), data)
 
 
-#second way to enter the wave file data (nframes + frame rate) using wavfile lib (100% work done but it is not filtered)
+
+#explaination: enter the wave file data (nframes + frame rate) using wavfile lib (100% work done but it is not filtered)
 fname = "test.wav"
 frate, data = wavfile.read(fname)
 
